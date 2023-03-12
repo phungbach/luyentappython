05744f7143264d042bff464617427c15de47a3e9
Phùng Bách <75926028+phungbach@users.noreply.github.com>
2023-03-13 00:36:23 +0700

    new file:   bt-coban/bt1.py 	renamed:    ls1-start-13-03-23/main.py -> bt-coban/bt2.py

---
+++ bt-coban/bt1.py
@@ -0,0 +1,8 @@
+'''
+Viết chương trình tính và hiển thị ra màn hình tổng hai số nguyên được nhập bất kỳ từ bàn phím.
+'''
+
+n = int(input("Nhập N: "))
+m = int(input("Nhập M: "))
+
+print(n,"+",m,"=",n+m)
